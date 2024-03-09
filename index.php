ba24549908e0908f65601ab9238658ca505a232a
GDNAGAL <128776145+GDNAGAL@users.noreply.github.com>
2024-03-09 10:23:13 +0530

    Update

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="Assets/Bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="Assets/Custom/Css/Style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="Assets/Bootstrap/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="main">
+        <div class="container-fluid" style="margin-top:50px; min-height:100vh; background:white">
+            <div class="position-fixed topnavbar">
+                <div class="navlogo">
+                    <img src="img/mainlogo.png" alt="">
+                </div>
+                <div class="content-right align-middle">
+                    <i class="fa fa-bell" aria-hidden="true"></i>
+                </div>
+            </div>
+            
+            <div class="d-block w-100">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style='top:10px'>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="img/Banners/1.png" class="d-block w-100 rounded-3" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/Banners/2.png" class="d-block w-100 rounded-3" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/Banners/3.png" class="d-block w-100 rounded-3" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-100 shadow-sm bg-body rounded-pill p-2 mt-3">
+                <i class="fa fa-bell" aria-hidden="true" style="color:#4781ff"></i>
+                <p class="d-inline p-0 ms-2 m-0 overflow-hidden" style="font-size:12px; width:60%; word-wrap: break-word">ksgdkjhkjskdhgjkhksjhdgj</p>
+                <button class="btn text-white rounded-pill float-end p-1 ps-3 pe-3" style="font-size:12px; background:#4781ff">Detail</button>
+            </div>
+
+            <h5 class="border-start border-5 ps-2 mt-3" >Lottery Games</h5>
+            <div class="border mt-3 rounded-3 p-3 gamelist">
+                <img src="img/wingologo.png" alt="" srcset="">
+            </div>
+        </div>
+    </div>
+</body>
+<script>
+</script>
+</html>
